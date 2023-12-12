430ea965d98ef2fce4024cc8a17c7622be90e555
Philip Andersson <philip.andersson.1991@hotmail.com>
2023-12-12 15:22:30 +0100

    Get notes up in UserNotes

--- src/Components/UserNotes.jsx
+++ src/Components/UserNotes.jsx
@@ -1,15 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 const UserNotes = () => {
 
+    const [message, setMessage] = useState("");
+    const [notes, setNotes] = useState([]);
     const userToken = useSelector(state => state.user.token);
+
+    const getNotesRequest = async () => {
+
+        const url = `https://pcd9bqvn13.execute-api.eu-north-1.amazonaws.com/api/notes`;
+
+        try {
+            const response = await fetch(url, {
+                method: "GET",
+                headers: {
+                    "Content-Type": 'application/json',
+                    Authorization: 'Bearer ' + userToken,
+                }
+            });
+
+            if(!response.ok) {
+                setMessage(`Error: server ${response.status}`);
+                return;
+            }
+
+            const responseJson = await response.json();
+
+            if (response.status === 200) {
+                setMessage(`fetched user notes successfully ${responseJson.notes}`);
+                setNotes(responseJson.notes);
+
+            } else {
+                setMessage(`Error: ${responseJson.message}`);
+            }
+        } catch (error) {
+            setMessage(`Error => catch ${error.message}`)
+        }
+    };
+
+    useEffect(() => {getNotesRequest();}, []);
 
 
   return (
     <div className='list' id='userNotesList'>
-        notes name 1 <br/>
-        notes name 2 <br/>
+        {notes.map((note, index) => 
+            <p key={index}>{note.title}</p>    
+        )}
         Token: <br/> <br/>
         {userToken}
     </div>
